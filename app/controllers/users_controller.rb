ad9edb20b91b41d9c545a50c5654b856cfca93c9
shin usuda <sasasa.24.blue@gmail.com>
2020-03-11 18:28:37 +0900

    Add create action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors
+    end
+  end
+
+  private
+
+  def user_params
+    prams.require(:user).permit(:name, :email, :uid)
+  end
+end
