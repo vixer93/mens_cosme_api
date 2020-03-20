77f51b05972ee9e2e6439dfe6d1cb4e97232d5a6
shin usuda <sasasa.24.blue@gmail.com>
2020-03-20 21:42:29 +0900

    Generate image model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_120424) do
+ActiveRecord::Schema.define(version: 2020_03_20_124022) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_images_on_product_id"
+  end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,5 +39,6 @@ ActiveRecord::Schema.define(version: 2020_03_20_120424) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "images", "products"
   add_foreign_key "products", "users"
 end
