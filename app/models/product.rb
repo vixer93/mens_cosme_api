efeb5a3124bc37e0d974cf2381339d5e37ed6ccc
shin usuda <sasasa.24.blue@gmail.com>
2020-03-20 22:40:50 +0900

    Remove presence true validation of point in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   has_many :images, dependent: :delete_all
   belongs_to :user
 
-  validates :name, :brand, :price, :point, presence: true
+  validates :name, :brand, :price, presence: true
 end
