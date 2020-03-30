bfd38f1de6c88770f496dc9df702e36ae2b28e36
vixer93 <59346949+vixer93@users.noreply.github.com>
2020-03-30 22:51:03 +0900

    商品詳細機能の実装 (#5)
    * Add show action in products controller
    
    * Fix image to url in json data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :users,    only: [:index, :create]
-  resources :products, only: [:index, :create]
+  resources :products, only: [:index, :show, :create]
 end
