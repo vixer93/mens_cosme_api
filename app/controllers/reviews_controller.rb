92632920ab14095d144dc3da8517b80819fd21ab
shin usuda <sasasa.24.blue@gmail.com>
2020-04-01 23:56:35 +0900

    Add create action in review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      render json: @review
+    else
+      render json: @review.errors.full_messages
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :point, :content, :user_id).merge(product_id: params[:product_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users,    only: [:index, :create]
-  resources :products, only: [:index, :show, :create]
+  resources :products, only: [:index, :show, :create] do
+    resources :reviews, only: [:create]
+  end
 end
