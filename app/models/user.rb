1a960ce06f0a1e74a68c521aa4a3a646a27f9285
shin usuda <sasasa.24.blue@gmail.com>
2020-04-11 22:57:13 +0900

    Add user authentic function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-            # Include default devise modules.
-            devise :database_authenticatable, :registerable,
-                    :recoverable, :rememberable, :trackable, :validatable,
-                    :confirmable, :omniauthable
-            include DeviseTokenAuth::Concerns::User
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable
+          # :confirmable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
   has_many :products
   has_many :reviews
 
-  validates :name, :email, :uid, presence: true
+  validates :name, :email, presence: true
 end
