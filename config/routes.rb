60203c9e2d6ab4b8f9d737cec3d51e7803dd54eb
shin usuda <sasasa.24.blue@gmail.com>
2020-04-11 18:17:42 +0900

    Install devise and devise token auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   resources :users,    only: [:index, :create]
   resources :products, only: [:index, :show, :create] do
     resources :reviews, only: [:index, :create]
